160d1e0d148d28aeaaa60e06df91b0a99b77aa6b
Deepali-Kundwani <74810444+Deepali-Kundwani@users.noreply.github.com>
2021-08-19 01:43:13 +0400

    Update useUrl.js

--- src/context/useUrl.js
+++ src/context/useUrl.js
@@ -2,7 +2,7 @@ import { useContext, createContext, useState } from "react";
 export const urlContext = createContext();
 
 export function UrlProvider({ children }) {
-  const [url, setUrl] = useState("https://api.rahulgupta99.repl.co/products");
+  const [url, setUrl] = useState("https://MyCart-Backend.codezillaa.repl.co/products");
   return (
     <urlContext.Provider value={{ url, setUrl }}>
       {children}
